7fe65ba0a1269cf90984768c8adad53b4e420a51
Zhangpeng <zhangpeng@mtedu.com>
2016-06-29 19:32:02 +0800

    完善models

--- models/logging/access_log.js
+++ models/logging/access_log.js
@@ -1,41 +1,41 @@
-/************************************************
-
-访问日志
-author: 张鹏
-date: 2016-06-22
-
-url: 用户访问的URL
-user_id: 用户ID, 如果用户未登录为0
-ip: 用户IP地址
-brower: 所使用的浏览器名称
-system: 所使用的操作系统
-device: 设备信息 电脑, 手机, 平板电脑....
-
-************************************************/
-
+/**
+ * AccessLog 访问日志
+ * @type {AccessLog}
+ * @author zp
+ * @version v1.0
+ */
 var AccessLog = sequlize.define('access_log', {
+    //用户访问的URL
     url: {
         type: Sequlize.STRING,
         field: 'url'
     },
+    //用户ID, 如果用户未登录为0
     user_id: {
         type: Sequlize.BIGINT,
         field: 'user_id'
     },
+    //用户IP地址 ipv4
     ip: {
-        type: Sequlize.STRING,
-        field: 'ip'
+        type: Sequlize.STRING(20),
+        field: 'ip',
+        validate: {
+            isIPv4: true
+        }
     },
+    //所使用的浏览器名称
     brower: {
-        type: Sequlize.STRING,
+        type: Sequlize.STRING(30),
         field: 'brower'
     },
+    //所使用的操作系统
     system: {
-        type: Sequlize.STRING,
+        type: Sequlize.STRING(50),
         field: 'system'
     },
+    //设备信息 电脑, 手机, 平板电脑....
     device: {
-        type: Sequlize.STRING,
+        type: Sequlize.STRING(50),
         field: 'device'
     }
 }, {
--- models/user/user.js
+++ models/user/user.js
@@ -1,54 +1,56 @@
-/*****************************************
-
- 用户中心
- author: 张鹏
- date: 2016-06-22
-
- username: 用户名
- nickname: 昵称
- password: 密码
- email: 电子邮件
- wechat_openid: 微信公众号的openid, 用于微信的绑定
- address: 用户地址
- gender: 性别
- avatar: 头像
-
-*****************************************/
+/**
+ * 用户中心
+ * @type {User}
+ * @author zp
+ * @version v1.0
+ */
 var Sequelize = require("sequelize");
 var sequelize = require('../index')
 User = sequelize.define('user', {
     username: {
-        type: Sequelize.STRING,
-        field: 'username'
+        type: Sequelize.STRING(50),
+        field: 'username',
+        validate: {
+            notNull: true
+        }
     },
     nickname: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(20),
         field: 'nickname'
     },
     password: {
-        type: Sequelize.STRING,
-        field: 'password'
+        type: Sequelize.STRING(50),
+        field: 'password',
+        validate: {
+            notNull: true
+        }
     },
     email: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(50),
         field: 'email'
     },
     mobile: {
-        type: Sequelize.STRING,
-        field: 'mobile'
+        type: Sequelize.STRING(20),
+        field: 'mobile',
+        validate: {
+            isNumeric: true
+        }
     },
+    //wechat_openid: 微信公众号的openid, 用于微信的绑定
     wechat_openid: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(30),
         field: 'wechat_openid'
     },
     address: {
         type: Sequelize.STRING,
         field: 'address'
     },
+    //性别
     gender: {
-        type: Sequelize.STRING,
+        type: Sequelize.BOOLEAN,
         field: 'gender'
     },
+    //头像
     avatar: {
         type: Sequelize.STRING,
         field: 'avatar'
