63350a544479ecbdc6a6e049af39c7afafb1b0c9
ZhangPeng <zp8360@qq.com>
2016-06-30 07:48:00 +0800

    access log record and session

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
+
+
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var useragent = require('useragent');
+var requestIp = require('request-ip');
+var AccessLog = require('./models/logging/access_log.js')
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var ejs = require('ejs');
-var engine = require('ejs-mate');
-
 
 var app = express();
 
@@ -25,16 +31,64 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'mengcms',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+
+app.use(function(req, res, next) {
+    var agent = useragent.parse(req.headers['user-agent']);
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    var clientIp = requestIp.getClientIp(req);
+
+    AccessLog.create({
+        brower: agent.toString(),
+        system: agent.os.toString(),
+        device: agent.device.toString(),
+        url: fullUrl,
+        ip: clientIp
+    }).then(function(created) {
+        console.log(JSON.stringify(created))
+    }).catch(function(err) {
+        console.error(err)
+    })
+
+    //session 拦截器
+    // if (req.session.user) { // 判断用户是否登录
+    //     next();
+    // } else {
+    //     // 解析用户请求的路径
+    //     var arr = req.url.split('/');
+    //     // 去除 GET 请求路径上携带的参数
+    //     for (var i = 0, length = arr.length; i < length; i++) {
+    //         arr[i] = arr[i].split('?')[0];
+    //     }
+    //     // 判断请求路径是否为根、登录、注册、登出，如果是不做拦截
+    //     if (arr.length > 1 && arr[1] == '') {
+    //         next();
+    //     } else if (arr.length > 2 && arr[1] == 'user' && (arr[2] == 'register' || arr[2] == 'login' || arr[2] == 'logout')) {
+    //         next();
+    //     } else { // 登录拦截
+    //         req.session.originalUrl = req.originalUrl ? req.originalUrl : null; // 记录用户原始请求路径
+    //         req.flash('error', '请先登录');
+    //         res.redirect('/user/login'); // 将用户重定向到登录页面
+    //     }
+    // }
+
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  console.log(req)
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -42,23 +96,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- models/logging/access_log.js
+++ models/logging/access_log.js
@@ -4,20 +4,22 @@
  * @author zp
  * @version v1.0
  */
-var AccessLog = sequlize.define('access_log', {
+var Sequelize = require("sequelize");
+var sequelize = require('../index')
+AccessLog = sequelize.define('access_log', {
     //用户访问的URL
     url: {
-        type: Sequlize.STRING,
+        type: Sequelize.STRING,
         field: 'url'
     },
     //用户ID, 如果用户未登录为0
     user_id: {
-        type: Sequlize.BIGINT,
+        type: Sequelize.BIGINT,
         field: 'user_id'
     },
     //用户IP地址 ipv4
     ip: {
-        type: Sequlize.STRING(20),
+        type: Sequelize.STRING(20),
         field: 'ip',
         validate: {
             isIPv4: true
@@ -25,19 +27,21 @@ var AccessLog = sequlize.define('access_log', {
     },
     //所使用的浏览器名称
     brower: {
-        type: Sequlize.STRING(30),
+        type: Sequelize.STRING(30),
         field: 'brower'
     },
     //所使用的操作系统
     system: {
-        type: Sequlize.STRING(50),
+        type: Sequelize.STRING(50),
         field: 'system'
     },
     //设备信息 电脑, 手机, 平板电脑....
     device: {
-        type: Sequlize.STRING(50),
+        type: Sequelize.STRING(50),
         field: 'device'
     }
 }, {
     freezeTableName: true
 })
+
+module.exports = AccessLog
